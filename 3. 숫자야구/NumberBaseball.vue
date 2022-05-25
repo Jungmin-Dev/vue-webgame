78ccce46b412165d5eaf09ae9ca667d23b5e42b3
Jungmin Kim <kimjungmin__@naver.com>
2022-05-25 17:44:48 +0900

    웹팩

---
+++ 3. 숫자야구/NumberBaseball.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div>{{result}}</div>
+        <form v-on:submit="onSubmitForm">
+            <input type="text" v-model="value" refs="answer"/>
+            <button type="submit">입력!</button>
+        </form>
+    <div> 시도:  </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            value: '',
+            result:''
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
