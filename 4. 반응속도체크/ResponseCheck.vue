b218cd064331e20e2b8d087a6cff75748c1846f4
Jungmin Kim <kimjungmin__@naver.com>
2022-05-30 14:41:49 +0900

    반응속도체크

---
+++ 4. 반응속도체크/ResponseCheck.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div id="screen" v-bind:class="state" v-on:click="onClickScreen">
+      {{ message }}
+    </div>
+    <div>
+      <div>
+        평균 시간 :
+        {{ result.reduce((a, c) => a + c, 0) / result.length || 0 }} ms
+      </div>
+      <button v-on:click="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+
+<script>
+let start = 0;
+let end = 0;
+let timeout = null;
+export default {
+  data() {
+    return {
+      result: [],
+      state: 'waiting',
+      message: '클릭해서 시작하세요.',
+    };
+  },
+  methods: {
+    onReset() {
+      this.result = [];
+    },
+    onClickScreen() {
+      if (this.state === 'waiting') {
+        this.state = 'ready';
+        this.message = '초록색이 되면 클릭하세요.';
+        timeout = setTimeout(() => {
+          this.state = 'now';
+          this.message = '지금 클릭하세요';
+          start = new Date();
+        }, Math.floor(Math.random() * 1000) + 2000);
+      } else if (this.state === 'ready') {
+        clearTimeout(timeout);
+        this.state = 'waiting';
+        this.message = '너무 성급하시군요! 초록색이 된 후에 클릭하세요.';
+      } else if (this.state === 'now') {
+        end = new Date();
+        this.state = 'waiting';
+        this.message = '클릭해서 시작하세요';
+        this.result.push(end - start);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+#screen.waiting {
+  background-color: aqua;
+}
+#screen.ready {
+  background-color: red;
+  color: white;
+}
+#screen.now {
+  background-color: greenyellow;
+}
+</style>
